f23a2d6ad559f0925ae0db2a1b506505c7270f81
llucasreis <lucas.lreis@mercadolivre.com>
2021-07-15 12:12:04 -0300

    feat: create domain entities

---
+++ seu-imovel-api/src/main/java/com/bootcamp/desafio/seuimovelapi/modules/properties/domain/District.java
@@ -0,0 +1,36 @@
+package com.bootcamp.desafio.seuimovelapi.modules.properties.domain;
+
+import java.math.BigDecimal;
+
+public class District {
+    private String prop_district;
+    private BigDecimal value_district_m2;
+
+    public District() {
+    }
+
+    public District(String prop_district) {
+        this.prop_district = prop_district;
+    }
+
+    public District(String prop_district, BigDecimal value_district_m2) {
+        this.prop_district = prop_district;
+        this.value_district_m2 = value_district_m2;
+    }
+
+    public String getProp_district() {
+        return prop_district;
+    }
+
+    public void setProp_district(String prop_district) {
+        this.prop_district = prop_district;
+    }
+
+    public BigDecimal getValue_district_m2() {
+        return value_district_m2;
+    }
+
+    public void setValue_district_m2(BigDecimal value_district_m2) {
+        this.value_district_m2 = value_district_m2;
+    }
+}
---
+++ seu-imovel-api/src/main/java/com/bootcamp/desafio/seuimovelapi/modules/properties/domain/Property.java
@@ -0,0 +1,58 @@
+package com.bootcamp.desafio.seuimovelapi.modules.properties.domain;
+
+import java.util.List;
+
+public class Property {
+    private Long id;
+    private String prop_name;
+    private District district;
+    private List<Room> rooms;
+
+    public Property() {
+    }
+
+    public Property(String prop_name, District district, List<Room> rooms) {
+        this.prop_name = prop_name;
+        this.district = district;
+        this.rooms = rooms;
+    }
+
+    public Property(Long id, String prop_name, District district, List<Room> rooms) {
+        this.id = id;
+        this.prop_name = prop_name;
+        this.district = district;
+        this.rooms = rooms;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProp_name() {
+        return prop_name;
+    }
+
+    public void setProp_name(String prop_name) {
+        this.prop_name = prop_name;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+}
