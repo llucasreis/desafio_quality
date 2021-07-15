99e29bf8f6e043c57b67f436df99a6ffe6ba2e41
llucasreis <lucas.lreis@mercadolivre.com>
2021-07-15 10:02:11 -0300

    chore: add initial project structure

---
+++ seu-imovel-api/src/main/java/com/bootcamp/desafio/seuimovelapi/SeuImovelApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.desafio.seuimovelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuImovelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuImovelApiApplication.class, args);
+	}
+
+}
---
+++ seu-imovel-api/src/test/java/com/bootcamp/desafio/seuimovelapi/SeuImovelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.desafio.seuimovelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuImovelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
