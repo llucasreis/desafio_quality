5dedfa1bd8fdcc2aeeced46c4383afba77e9c585
llucasreis <lucas.lreis@mercadolivre.com>
2021-07-16 10:46:25 -0300

    feat: create ConflictException

--- seu-imovel-api/src/main/java/com/bootcamp/desafio/seuimovelapi/shared/handlers/ApiExceptionHandler.java
+++ seu-imovel-api/src/main/java/com/bootcamp/desafio/seuimovelapi/shared/handlers/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.bootcamp.desafio.seuimovelapi.shared.handlers;
 
 import com.bootcamp.desafio.seuimovelapi.shared.errors.ErrorResponse;
+import com.bootcamp.desafio.seuimovelapi.shared.exceptions.ConflictException;
 import com.bootcamp.desafio.seuimovelapi.shared.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,12 @@ public class ApiExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ErrorResponse<String> handleNotFoundException(NotFoundException exception) {
+        return new ErrorResponse<>(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ConflictException.class)
+    public ErrorResponse<String> handleConflictException(ConflictException exception) {
         return new ErrorResponse<>(exception.getMessage());
     }
 }
