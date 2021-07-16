87d4fb5b5e8ef431ea7de6004c902119321cdf0d
llucasreis <lucas.lreis@mercadolivre.com>
2021-07-16 10:46:43 -0300

    feat: create new unit tests for district service

--- seu-imovel-api/src/test/java/com/bootcamp/desafio/seuimovelapi/unit/districts/services/DistrictServiceImplTest.java
+++ seu-imovel-api/src/test/java/com/bootcamp/desafio/seuimovelapi/unit/districts/services/DistrictServiceImplTest.java
@@ -3,10 +3,12 @@ package com.bootcamp.desafio.seuimovelapi.unit.districts.services;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.bootcamp.desafio.seuimovelapi.modules.districts.domain.District;
+import com.bootcamp.desafio.seuimovelapi.modules.districts.dtos.DistrictFormDTO;
 import com.bootcamp.desafio.seuimovelapi.modules.districts.repositories.DistrictRepository;
 import com.bootcamp.desafio.seuimovelapi.modules.districts.repositories.DistrictRepositoryImpl;
 import com.bootcamp.desafio.seuimovelapi.modules.districts.services.DistrictService;
 import com.bootcamp.desafio.seuimovelapi.modules.districts.services.DistrictServiceImpl;
+import com.bootcamp.desafio.seuimovelapi.shared.exceptions.ConflictException;
 import com.bootcamp.desafio.seuimovelapi.shared.exceptions.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +28,34 @@ public class DistrictServiceImplTest {
         districtRepository = Mockito.mock(DistrictRepositoryImpl.class);
 
         districtService = new DistrictServiceImpl(districtRepository);
+    }
+
+    @Test
+    public void shouldBeAbleToCreateADistrict() {
+        Mockito.when(districtRepository.findByName(ArgumentMatchers.any(String.class))).thenReturn(null);
+        Mockito.when(districtRepository.createDistrict(ArgumentMatchers.any(District.class))).thenReturn(true);
+
+        DistrictFormDTO formDTO = new DistrictFormDTO("Japiim", BigDecimal.valueOf(2910.7));
+
+        boolean response = this.districtService.createDistrict(formDTO);
+
+        Assertions.assertThat(response).isEqualTo(true);
+    }
+
+    @Test
+    public void shouldNotBeAbleToCreateADistrictIfAlreadyExist() {
+        String expected = "Bairro já existe";
+
+        District mockDistrict = new District(1L, "Japiim", BigDecimal.valueOf(2910.7));
+        Mockito.when(districtRepository.findByName(ArgumentMatchers.any(String.class))).thenReturn(mockDistrict);
+
+        DistrictFormDTO formDTO = new DistrictFormDTO("Japiim", BigDecimal.valueOf(2910.7));
+
+        Exception exception = assertThrows(ConflictException.class, () -> {
+            this.districtService.createDistrict(formDTO);
+        });
+
+        Assertions.assertThat(exception.getMessage()).isEqualTo(expected);
     }
 
     @Test
